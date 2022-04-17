799d9f13e18a76e006eed110a56ee14c7bceeeb7
DanielLucasL <72170522+DanielLucasL@users.noreply.github.com>
2022-04-17 13:17:58 -0300

    Commit 1 -TDD

---
+++ TDDBoletos-main/TDDBoletos-main/README.md
@@ -0,0 +1,1 @@
+# TDDBoletos
---
+++ TDDBoletos-main/TDDBoletos-main/TDDBoletos/src/Boleto/Boleto.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Boleto;
+
+/**
+ *
+ * @author Aluno
+ */
+class Boleto extends Pagamento{
+
+    void setcodigo(String codigo) {
+      
+    }
+
+    Object getcodigo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+
+
+    
+
+    
+    
+}
